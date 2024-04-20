f65fd768b7454e78dd12ccc996b9d9bcdf85e168
Ukjong <xb253q@gmail.com>
2024-04-20 14:25:28 +0900

    feat: UserService 추가 - 로그인 - 회원가입

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,2 +1,30 @@
-package com.cos.blog.service;public class UserService {
+package com.cos.blog.service;
+
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+// 스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IoC를 해줌.
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public int join(User user) {
+        try {
+            userRepository.save(user);
+            return 1;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    @Transactional(readOnly = true) // select할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료(정합성)
+    public User login(User user) {
+        return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
+    }
 }
