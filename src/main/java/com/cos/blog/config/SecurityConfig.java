962685db0ac304abf5dccbf1d914ac2d84dc2bdb
Ukjong <xb253q@gmail.com>
2024-04-21 00:09:16 +0900

    refactor: csrf에 대한 주석작성

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
             .csrf((csrf) -> csrf
-                .disable())
+                .disable()) // csrf 토큰 비활성화 (테스트시 걸어두는 게 좋음)
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**").permitAll()
                 .anyRequest().authenticated()
