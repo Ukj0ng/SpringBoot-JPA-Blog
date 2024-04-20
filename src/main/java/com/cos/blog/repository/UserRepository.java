e8ba70248027b6fd68a4bb4a7d875d6a971cd8b6
Ukjong <xb253q@gmail.com>
2024-04-20 23:39:39 +0900

    fix: security login을 위해 함수 주석처리

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -7,10 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 // DAO
 // 자동으로 bean으로 등록이 됨. -> @Repository 생략 가능
 public interface UserRepository extends JpaRepository<User, Integer> {
-    // JPA Naming 쿼리
-    // select * from user where username = ? and password = ?;
-    User findByUsernameAndPassword(String username, String password);
-
-//    @Query(value = "select * from user where username = ?1 and password = ?2", nativeQuery = true)
-//    User login(String username, String password);
 }
+
+// JPA Naming 쿼리
+// select * from user where username = ? and password = ?;
+// User findByUsernameAndPassword(String username, String password);
+
+// @Query(value = "select * from user where username = ?1 and password = ?2", nativeQuery = true)
+// User login(String username, String password);
